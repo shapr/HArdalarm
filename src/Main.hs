89c6a662024707259369c208519f5aa6fb4cdedd
shapr <shae@ScannedInAvian.com>
2013-01-28 21:41:00 -0600

    add pitches.h to the arduino sketch, create Alarm datatype, fail to figure out Data.Colour

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,5 @@
 import           Moodclock
 
-
 import           Control.Concurrent    (threadDelay)
 import           Data.DateTime
 import           System.IO
@@ -10,11 +9,11 @@ import           System.Serial
 main = do
      s <- openSerial serialport B9600 8 One NoParity NoFlowControl
      let r = 255;g = 255; b = 255 -- ; sPut = setColor s
-     hPutStr s "r5 b5 g0"
+     hPutStr s "r5 b5 g5 "
      hFlush s
      -- threadDelay 300000
      -- hPutStr s "r25 b24 g25"
-     mapM (\x -> setColor s x x x) [1,15..255]
+     mapM (\x -> setColor s x x x) ([1,50..255])
      -- setColor s 0 0 0 >> hFlush s
      hPutStr s " b0 g0 r0"
      hFlush s
--- src/Moodclock.hs
+++ src/Moodclock.hs
@@ -1,11 +1,25 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Moodclock where
 
 import           Control.Concurrent    (threadDelay)
-import           Data.Colour
+import qualified Data.Colour           as DC
 import           Data.DateTime
 import           System.IO
 import           System.Posix.Terminal
 import           System.Serial
+import           System.Time
+
+
+newtype Hour = Hour Int deriving (Eq,Ord,Show,Num)-- 0-23
+newtype Minute = Minute Int deriving (Eq,Ord,Show,Num) -- 0-59
+-- data System.Time.Day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
+data Alarm = Alarm Day Hour Minute AlarmAction deriving (Eq,Ord,Show)
+
+-- Data.Colour from colour should not shadow Data.Colour from AC-Colour! ARGH FRUSTRATION!
+data AlarmAction = Color | Tone deriving (Eq,Ord,Show)
+
+alarms :: [Alarm]
+alarms = [Alarm Monday 20 50 Color]
 
 serialport = "/dev/ttyUSB0"
 
@@ -24,10 +38,19 @@ mymain = do
      putStrLn "We're done!"
 
 setColor  :: (Show a, Show a1, Show a2) => Handle -> a2 -> a1 -> a -> IO ()
-{-# NOINLINE setColor #-}
-setColor s r g b = hPutStr s actual >> hFlush s >> putStrLn actual >> threadDelay 300000
+setColor s r g b = do
+         hPutStr s actual
+         hFlush s
+         -- display output from the Arduino
+         (hGetLine s >>= putStrLn)
+         (hGetLine s >>= putStrLn)
+         (hGetLine s >>= putStrLn)
+         (hGetLine s >>= putStrLn)
+         threadDelay 300000
          where sr = "r" ++ show r
                sg = "g" ++ show g
                sb = "b" ++ show b
                actual = (" " ++ sr ++ " " ++ sg ++ " " ++ sb ++ " ")
 
+
+-- checkAlarms :: [Alarms] -> CalendarTime ->
